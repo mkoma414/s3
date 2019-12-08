46461506ec6d1bb17b5eef8fe0380eacdcffdee4
Komatsubara Masato <play.with.loa@gmail.com>
2019-12-08 14:30:10 +0900

    s3のバケット指定を修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,11 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 's3-app-414'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/s3-app-414'
+  # config.fog_directory  = 's3-app-414'
+  # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/s3-app-414'
+  config.fog_directory  = 'chat-space2019'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space2019'
 end
+
+
+
